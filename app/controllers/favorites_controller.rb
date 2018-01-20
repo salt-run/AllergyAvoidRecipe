a99e7ad0f819dd399e82f8d958fb7193017541c0
Hideto Shiozawa <salt-run@Hidetos-MacBook-Pro.local>
2018-01-21 00:52:18 +0900

    error_correction

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.create(recipe_id: params[:recipe_id])
     #redirect_to recipes_url, noteic: "#{favorite.recipe.user.name}さんのレシピをお気に入り登録しました"
     recipe = Recipe.find(params[:recipe_id])
-    recipe.favorite_count =+1 
+    recipe.favorite_count += 1
     recipe.update(:favorite_count => recipe.favorite_count)
     redirect_to recipe_path(params[:recipe_id]), noteic: "#{favorite.recipe.user.name}さんのレシピをお気に入り登録しました"
   end
@@ -13,7 +13,7 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(recipe_id: params[:recipe_id]).destroy
     #redirect_to recipes_url, notice: "#{favorite.recipe.user.name}さんのブログをお気に入り解除しました"
     recipe = Recipe.find(params[:recipe_id])
-    recipe.favorite_count =- 1
+    recipe.favorite_count -= 1
     recipe.update(:favorite_count => recipe.favorite_count)
     redirect_to recipe_path(params[:recipe_id]), notice: "#{favorite.recipe.user.name}さんのレシピをお気に入り解除しました"
   end
